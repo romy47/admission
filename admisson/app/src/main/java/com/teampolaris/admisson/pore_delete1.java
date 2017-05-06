a2c62ecc2ea3fc42eaa2b72a25598c1cc2eaf85a
romy <romy6047@gmail.com>
2017-05-06 11:27:04 +0600

    all_files_added

---
+++ admisson/app/src/main/java/com/teampolaris/admisson/pore_delete1.java
@@ -0,0 +1,36 @@
+package com.teampolaris.admisson;
+
+/**
+ * Created by User on 05/05/2017.
+ */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Belal on 18/09/16.
+ */
+
+
+public class pore_delete1 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.pore_delete1, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Menu 1");
+    }
+}
+
